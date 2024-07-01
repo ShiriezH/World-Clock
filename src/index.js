40e2cd1232d6015f85c1e593572890a4a8f3bf5c
Lady <109853422+ShiriezH@users.noreply.github.com>
2024-07-01 18:14:15 +0200

    Worked on the JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+setInterval(function () {
+  // Rotterdam
+  let rotterdamElement = document.querySelector("#rotterdam");
+  let rotterdamDateElement = rotterdamElement.querySelector(".date");
+  let rotterdamTimeElement = rotterdamElement.querySelector(".time");
+  let rotterdamTime = moment().tz("Netherland/Rotterdam");
 
+  rotterdamDateElement.innerHTML = rotterdamTime.format("MMMM Do YYYY");
+  rotterdamTimeElement.innerHTML = rotterdamTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
